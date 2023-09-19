46a115726096c10efcd9400424f56dc3cad09903
sound <chaki.bubalo@gmail.com>
2023-09-19 21:50:14 +0500

    Add code

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTest {
@@ -16,7 +17,7 @@ public class DragAndDropTest {
     @Test
     void dragAndDrop(){
         open("/drag_and_drop");
-        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").dragAndDrop(to ("#column-b"));
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
     }
